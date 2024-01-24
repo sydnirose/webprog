ef36311ed5969cc0fd5ededb2ff282586729a4bd
sydnirose <salexandra.rose@gmail.com>
2024-01-24 10:15:45 -0600

    wk 2 exer 1

---
+++ wk2/2.1_moviearray/moviearray.js
@@ -0,0 +1,6 @@
+let movie = window.prompt("What is your favorite movie?");
+console.log(movie);
+
+let myArr = ['Spirited Away', 'Call Me By Your Name', 'Harry Potter', 'Saltburn', 'The Lion King'];
+console.log(myArr);
+myArr.push(movie);
