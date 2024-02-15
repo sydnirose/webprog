fa461468f0fc90a6fa918a0c46dc97078a67ace2
sydnirose <salexandra.rose@gmail.com>
2024-02-15 15:49:56 -0600

    updates

--- wk5/5.2_classesandbubble/classes.js
+++ wk5/5.2_classesandbubble/classes.js
@@ -1,31 +1,16 @@
 // variables
-let bubble1;
-let bubble2;
-let bubble3;
-let bubble4;
-let bubble5;
-let bubble6;
-
 let bubbles = [];
 let numBubbles = 18;
 
 function setup() {
     createCanvas(600, 600);
-    bubble1 = new Bubble();
-    bubble2 = new Bubble();
-    bubble3 = new Bubble();
-    bubble4 = new Bubble();
-    bubble5 = new Bubble();
-    bubble6 = new Bubble();
-
-    for(let i = 0;i<numBubbles; i++) {
+    for(let i = 0; i<numBubbles; i++) {
         bubbles[i] = new Bubble();
     }
 }
 
 function draw() {
     background('#112063');
-
     for(let i = 0; i<bubbles.length; i++) {
         bubbles[i].move();
         bubbles[i].show();
@@ -58,14 +43,3 @@ class Bubble {
         ellipse(this.x, this.y, 64, 64);
     }
 }
-
-
-
-
-
-// show() {
-//     stroke('white');
-//     strokeWeight(4);
-//     noFill();
-//     ellipse(this.x, this.y, 44, 44);
-// }
