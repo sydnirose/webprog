b60836cd8de09a9bd9d05aa08a3b8e5a5b4832ca
sydnirose <salexandra.rose@gmail.com>
2024-02-18 13:55:14 -0600

    more progress

--- wk5/5.a_flowermemory/flowermemory.js
+++ wk5/5.a_flowermemory/flowermemory.js
@@ -5,7 +5,11 @@ let startingX = 50;
 let startingY = 50;
 let cards = [];
 const gameState = {
-
+    totalPairs: 8, 
+    flippedCards: [],
+    numMatched: 0,
+    attempts: 0,
+    waiting: false
 };
 let cardfaceArray = [];
 let cardback;
@@ -25,8 +29,7 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(700, 700);
-    background('#f5ecd0');
+    createCanvas(700, 900);
     let selectedFaces = [];
     for (let z = 0; z < 8; z++) {
         const randomIdx = floor(random(cardfaceArray.length));
@@ -47,10 +50,52 @@ function setup() {
     }
 }
 
+function draw() {
+    background('#f5ecd0');
+    if (gameState.numMatch === gameState.totalPairs) {
+        fill('green');
+        textSize(66);
+        text('You win!!', 400, 425);
+        noLoop();
+    }
+    for (let l = 0; l < cards.length; l++) {
+        if(!cards[l].isMatch) {
+            cards[l].face = DOWN;
+        }
+        cards[l].show();
+    }
+    noLoop();
+    gameState.flippedCards.length = 0;
+    gameState.waiting = false;
+    fill('#756046');
+    textSize(36);
+    text('Attempts ' + gameState.attempts, 50, 750);
+    text('Matches ' + gameState.numMatched, 50, 700);
+}
+
 function mousePressed() {
+    if (gameState.waiting) {
+        return;
+    }
     for (let k = 0; k < cards.length; k++) {
-        if(cards[k].didHit(mouseX, mouseY)) {
+        if (gameState.flippedCards.length < 2 && cards[k].didHit(mouseX, mouseY)) {
             console.log('flipped', cards[k]);
+            gameState.flippedCards.push(cards[k]);
+        }
+    }
+    if (gameState.flippedCards.length === 2) {
+        if (gameState.flippedCards[0].faceImage === gameState.flippedCards[1].faceImage) {
+            gameState.flippedCards[0].isMatch = true;
+            gameState.flippedCards[1].isMatch = true;
+            gameState.flippedCards.length = 0;
+            gameState.numMatched++;
+            loop();
+        } else {
+            gameState.waiting = true;
+            const loopTimeout = window.setTimeout(() => {
+                loop();
+                window.clearTimeout(loopTimeout);
+            }, 1000)
         }
     }
 }
@@ -63,20 +108,21 @@ class Card {
         this.height = 140;
         this.face = DOWN;
         this.cardFaceImg = cardFaceImg;
+        this.isMatch = false;
         this.show();
     }
 
     show () {
-        if(this.face === DOWN) {
-            fill('#1b6627');
-            noStroke();
-            rect(this.x, this.y, this.width, this.height, 10);
-            image(cardback, this.x, this.y); 
-        } else {
+        if(this.face === UP || this.isMatch) {
             fill('#e074d6');
             noStroke();
             rect(this.x, this.y, this.width, this.height, 10);
             image(this.cardFaceImg, this.x, this.y);
+        } else {
+            fill('#1b6627');
+            noStroke();
+            rect(this.x, this.y, this.width, this.height, 10);
+            image(cardback, this.x, this.y); 
         }
     }
 
