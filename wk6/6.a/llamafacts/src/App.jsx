aaf38b2021f4ddce6d9d7230866aeb1aff0619bc
sydnirose <salexandra.rose@gmail.com>
2024-02-24 16:59:47 -0600

    6.a

---
+++ wk6/6.a/llamafacts/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  const llamaFacts = [
+    "Llama's are related to the Camel. ", 
+    "Llama's are native to the mountainous region of South America. ",
+    "Female llama's give birth every other year. ",
+    "When a llama is angry at a fellow llama, they stick their tongues out to express their dislike. ",
+    "A llama can project it's spit 15 or more feet. ", 
+  ];
+
+  return (
+    <>
+      <h1>LLAMA FACTS! 🦙👨🏻‍🌾</h1>
+      <div>
+      {llamaFacts}
+      {llamaFacts ? <div>When a llama is angry at a fellow llama, they stick their tongues out to express their dislike.</div> : <div>A llama can project it's spit 15 or more feet. </div>}
+      </div>
+    </>
+  )
+}
+
+export default App
