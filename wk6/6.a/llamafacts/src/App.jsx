3ba13417f7d88a20b631d926f2f00456c39bddb8
sydnirose <salexandra.rose@gmail.com>
2024-02-24 17:12:18 -0600

    6.a

--- wk6/6.a/llamafacts/src/App.jsx
+++ wk6/6.a/llamafacts/src/App.jsx
@@ -6,15 +6,18 @@ function App() {
     "Llama's are native to the mountainous region of South America. ",
     "Female llama's give birth every other year. ",
     "When a llama is angry at a fellow llama, they stick their tongues out to express their dislike. ",
-    "A llama can project it's spit 15 or more feet. ", 
+    "A llama can project their spit 15 or more feet. ", 
   ];
 
   return (
     <>
       <h1>LLAMA FACTS! 🦙👨🏻‍🌾</h1>
       <div>
+      {/* this shows all facts */}
       {llamaFacts}
-      {llamaFacts ? <div>When a llama is angry at a fellow llama, they stick their tongues out to express their dislike.</div> : <div>A llama can project it's spit 15 or more feet. </div>}
+
+      {/* this shows one of last two */}
+      {llamaFacts ? <div>When a llama is angry at a fellow llama, they stick their tongues out to express their dislike.</div> : <div>A llama can project their spit 15 or more feet. </div>}
       </div>
     </>
   )
