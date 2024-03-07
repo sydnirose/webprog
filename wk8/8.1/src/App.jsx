8ffde16a9ba0cd2671c85ae076f7f997f28189e5
sydnirose <salexandra.rose@gmail.com>
2024-03-06 20:21:09 -0600

    8.1

--- wk8/8.1/src/App.jsx
+++ wk8/8.1/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import llama from './assets/llama.png';
+import alpaca from './assets/alpaca.png';
+import Animal from './Animal';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pet, setPet] = useState(llama)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Animal url={pet} />
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => {
+          console.log("click llama");
+          setPet(llama);
+        }}>
+          Llama
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+        <button onClick={() => {
+          console.log("click alpaca");
+          setPet(alpaca);
+        }}>
+          Alpaca
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
