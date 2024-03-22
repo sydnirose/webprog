db0f502fb7335c20c3fcc793b87a4227d84413b9
sydnirose <salexandra.rose@gmail.com>
2024-03-21 20:37:03 -0500

    9.2

---
+++ wk9/9.2/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { AlpacaForm } from './AlpacaForm'
+
+function App() {
+
+  return (
+    <>
+    <h1>Alpaca Fan Club Registration </h1>
+    <AlpacaForm />
+    </>
+  )
+}
+
+export default App
