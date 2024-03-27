f8a85a6b35726c7fb4769b2ba6b47cd05b8158d4
sydnirose <salexandra.rose@gmail.com>
2024-03-27 13:30:44 -0500

    10.1

--- wk10/10.1/src/App.jsx
+++ wk10/10.1/src/App.jsx
@@ -1,10 +1,27 @@
+import { Link, Route, Routes } from 'react-router-dom'
+import { Home } from './views/Home'
+import { Llama } from './views/Llama'
+import { Alpaca } from './views/Alpaca'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     hi
+      {/* nav here */}
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/llama">Llamas</Link></li>
+        <li><Link to="/alpaca">Alpacas</Link></li>
+      </ul>
+
+      {/* Routes here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/llama" element={<Llama />} />
+        <Route path="/alpaca" element={<Alpaca />} />
+
+      </Routes>
     </>
   )
 }
