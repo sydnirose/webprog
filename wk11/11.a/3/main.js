a9aa4e29da17c773355cc695deb46be48512217c
sydnirose <salexandra.rose@gmail.com>
2024-04-11 11:41:03 -0500

    11.a

---
+++ wk11/11.a/3/main.js
@@ -0,0 +1,49 @@
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    background('#d8d8e3');
+    noStroke();
+    fill('#032363');
+
+    // star 1
+    push();
+    translate(width * 0.8, height * 0.5);
+    star(0, -100, 30, 70, 10);
+    pop();
+
+    // star 2
+    push();
+    translate(width * 0.8, height * 0.5);
+    star(-200, 30, 30, 70, 10);
+    pop();
+    
+    // star 3
+    push();
+    translate(width * 0.8, height * 0.5);
+    star(-400, -200, 30, 70, 10);
+    pop();
+
+    // star 4
+    push();
+    translate(width * 0.8, height * 0.5);
+    star(-500, 150, 30, 70, 10);
+    pop();
+}
+
+// star shape
+function star(x, y, radius1, radius2, npoints) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+        beginShape();
+            for (let a = 0; a < TWO_PI; a += angle) {
+                let sx = x + cos(a) * radius2;
+                let sy = y + sin(a) * radius2;
+                vertex(sx, sy);
+                sx = x + cos(a + halfAngle) * radius1;
+                sy = y + sin(a + halfAngle) * radius1;
+                vertex(sx, sy);
+            }
+        endShape(CLOSE);
+}
